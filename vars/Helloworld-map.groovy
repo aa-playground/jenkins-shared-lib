f7fb0770e8470b58302ab12252c78a41e7f1b01a
Alex Anto <alexantokp@gmail.com>
2021-12-11 22:54:15 +0530

    Update Helloworld-map.groovy

--- vars/Helloworld-map.groovy
+++ vars/Helloworld-map.groovy
@@ -1,5 +1,3 @@
-#Define empty map
-
-def call(Map config = [:]){
-  sh "echo Hello ${config.name}". Today is ${config.dayofweek}."
+def call(Map config = [:]) {
+  sh "echo Hello ${config.name}. Today is ${config.dayofweek}."
 }
